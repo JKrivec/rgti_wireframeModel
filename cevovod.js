7fe39318521fe897df2ddea8bec2401d049dd339
unknown <jan.krivecc@gmail.com>
2019-10-23 22:53:19 +0200

    neki dela napol

--- cevovod.js
+++ cevovod.js
@@ -1,8 +1,9 @@
-var vozlisca = [];
-var povezave = [];
-
-
-
+var vozlisca3d = [];
+var povezave3d = [];
+var razdaljaKamere = 50;
+var platno = document.getElementById("platno")
+var context = platno.getContext("2d");
+var sredinskaTocka = [];
 
 //branje datoteke uploadane na stran
 function naloziDatoteko(){
@@ -12,41 +13,142 @@ function naloziDatoteko(){
 
     reader.onload = function(){
         var text =  reader.result.split("\n");      
-
+        console.log("lenght text : "+text.length);
+        var stVozl = 0;
+        var stPove = 0;
         for(var i = 0; i < text.length; i++){
-            var stVozl = 0;
-            var stPove = 0;
             var vrstica = text[i].split(" ");
           
-                if(vrstica[0] == 'v'){
-                    vozlisca[stVozl] = [];
-                    vozlisca[stVozl][0] = vrstica[1];
-                    vozlisca[stVozl][1] = vrstica[2];
-                    vozlisca[stVozl][2] = vrstica[3]; 
-                    stVozl++;
-                }else if(vrstica[0] == 'f'){
-                    povezave[stPove] = [];
-                    povezave[stPove][0] = vrstica[1];
-                    povezave[stPove][1] = vrstica[2];
-                    povezave[stPove][2] = vrstica[3]; 
-                    stPove++;
-                }
-
-            
-            
-
-            
-            
-
-            
-            
+            if(vrstica[0] == 'v'){
+                vozlisca3d[stVozl] = [];
+                vozlisca3d[stVozl][0] = parseFloat(vrstica[1]);
+                vozlisca3d[stVozl][1] = parseFloat(vrstica[2]);
+                vozlisca3d[stVozl][2] = parseFloat(vrstica[3]);
+                stVozl++;
+            }else if(vrstica[0] == 'f'){
+                povezave3d[stPove] = [];
+                povezave3d[stPove][0] = parseFloat(vrstica[1]);
+                povezave3d[stPove][1] = parseFloat(vrstica[2]);
+                povezave3d[stPove][2] = parseFloat(vrstica[3]);
+                stPove++;
+            }        
         }
-        console.log(vozlisca);
-        document.getElementById("text").innerHTML = vozlisca[0];
-        document.getElementById("text2").innerHTML = vozlisca[1];
-
     }
     reader.readAsText(file);
-      
-    
 }
+
+/*
+function izracunajSredinskoTocko(){
+    var vsotaX = 0.0;
+    var vsotaY = 0.0;
+    var vsotaZ = 0.0;
+    var stTock= vozlisca3d.length;
+    for(var i = 0; i < stTock; i++){
+        vsotaX+= vozlisca3d[i][0];
+        vsotaY+= vozlisca3d[i][1];
+        vsotaZ+= vozlisca3d[i][2];
+    }
+
+    sredinskaTocka[0] = vsotaX / stTock;
+    sredinskaTocka[1] = vsotaY / stTock;
+    sredinskaTocka[2] = vsotaZ / stTock;
+}
+*/
+
+
+
+
+
+
+var refreshId = setInterval(function(){
+    rotateY(0.01);
+    rotateX(0.01);
+    izris();
+
+}, 15);
+
+
+
+
+function rotateX(kot){
+    //izracunajSredinskoTocko();
+    for(var i = 0; i < vozlisca3d.length; i++){
+        var x = vozlisca3d[i][0];
+        var y = vozlisca3d[i][1];
+        var z = vozlisca3d[i][2];
+        vozlisca3d[i][1] = y*Math.cos(kot) - z*Math.sin(kot);
+        vozlisca3d[i][2] = y*Math.sin(kot) + z*Math.cos(kot);
+
+    }
+}
+function rotateY(kot){
+    for(var i = 0; i < vozlisca3d.length; i++){
+        var x = vozlisca3d[i][0];
+        var y = vozlisca3d[i][1];
+        var z = vozlisca3d[i][2];
+        vozlisca3d[i][0] = z*Math.cos(kot) - x*Math.sin(kot);
+        vozlisca3d[i][2] = z*Math.sin(kot) + x*Math.cos(kot);
+    }
+}
+function rotateZ(kot){
+    for(var i = 0; i < vozlisca3d.length; i++){
+        var x = vozlisca3d[i][0];
+        var y = vozlisca3d[i][1];
+        var z = vozlisca3d[i][2];
+        vozlisca3d[i][0] = x*Math.cos(kot) - y*Math.sin(kot);
+        vozlisca3d[i][1] = x*Math.sin(kot) + y*Math.cos(kot);
+    }
+}
+
+function izris(){
+    context.clearRect(0, 0, 1280, 720);//pobrise prejsno sliko
+    var odmikX = platno.width/2;
+    var odmikY = platno.height/2; 
+
+    var vozlisca2d = [];
+    var stVozlisc = 0;
+    //preslikava v 2d prostor
+    for(var i = 0; i < vozlisca3d.length; i++){
+        vozlisca2d[stVozlisc] = [];
+        var x = vozlisca3d[stVozlisc][0];
+        var y = vozlisca3d[stVozlisc][1];
+        var z = vozlisca3d[stVozlisc][2];
+        if(z == 0){
+            vozlisca2d[stVozlisc][0] = x * razdaljaKamere;
+            vozlisca2d[stVozlisc][1] = y * razdaljaKamere;
+            //console.log("2d x= " +vozlisca2d[stVozlisc][0]);
+            //console.log("2d y= " +vozlisca2d[stVozlisc][1]);
+        }else{
+            vozlisca2d[stVozlisc][0] = x * (razdaljaKamere/z);
+            //console.log("2d x= " +vozlisca2d[stVozlisc][0]);
+            vozlisca2d[stVozlisc][1] = y * (razdaljaKamere/z);  
+            //console.log("2d y= " +vozlisca2d[stVozlisc][1]); 
+        }
+        
+        stVozlisc++;
+    }
+
+    //risanje
+    context.beginPath();
+    for(var i = 0; i < povezave3d.length; i++){
+        context.moveTo(vozlisca2d[povezave3d[i][0]-1][0] +odmikX , -vozlisca2d[povezave3d[i][0]-1][1]+odmikY );
+        context.lineTo(vozlisca2d[povezave3d[i][1]-1][0] +odmikX , -vozlisca2d[povezave3d[i][1]-1][1]+odmikY );
+       
+        context.moveTo(vozlisca2d[povezave3d[i][1]-1][0] +odmikX , -vozlisca2d[povezave3d[i][1]-1][1]+odmikY );
+        context.lineTo(vozlisca2d[povezave3d[i][2]-1][0] +odmikX , -vozlisca2d[povezave3d[i][2]-1][1]+odmikY );
+        
+        context.moveTo(vozlisca2d[povezave3d[i][2]-1][0] +odmikX , -vozlisca2d[povezave3d[i][2]-1][1]+odmikY );
+        context.lineTo(vozlisca2d[povezave3d[i][0]-1][0] +odmikX , -vozlisca2d[povezave3d[i][0]-1][1]+odmikY );
+        
+       
+    }
+    context.closePath();
+    context.stroke();
+    //rotateX(0.2);
+    
+    
+    
+
+
+}
+
