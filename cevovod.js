f85ff5cdf4c3cc415a80337d4a39a74750ab8093
unknown <jan.krivecc@gmail.com>
2019-10-29 15:22:34 +0100

    skor dela

--- cevovod.js
+++ cevovod.js
@@ -1,9 +1,13 @@
-var vozlisca3d = [];
+var vozlisca4d = [];
 var povezave3d = [];
-var razdaljaKamere = 50000;
+var razdaljaKamere = 8;
+var kamera = [0,0,8];
 var platno = document.getElementById("platno")
 var context = platno.getContext("2d");
-var sredinskaTocka = [];
+var rotirajX = false;
+var rotirajY = false;
+var rotirajZ = false;
+
 
 //branje datoteke uploadane na stran
 function naloziDatoteko(){
@@ -13,18 +17,18 @@ function naloziDatoteko(){
 
     reader.onload = function(){
         var text =  reader.result.split("\n");      
-        console.log("lenght text : "+text.length);
         var stVozl = 0;
         var stPove = 0;
         for(var i = 0; i < text.length; i++){
             var vrstica = text[i].split(" ");
           
             if(vrstica[0] == 'v'){
-                vozlisca3d[stVozl] = [];
-                vozlisca3d[stVozl][0] = parseFloat(vrstica[1]);
-                vozlisca3d[stVozl][1] = parseFloat(vrstica[2]);
-                vozlisca3d[stVozl][2] = parseFloat(vrstica[3]);
-                stVozl++;
+                vozlisca4d[stVozl] = [];
+                vozlisca4d[stVozl][0] = parseFloat(vrstica[1]);
+                vozlisca4d[stVozl][1] = parseFloat(vrstica[2]);
+                vozlisca4d[stVozl][2] = parseFloat(vrstica[3]);
+                vozlisca4d[stVozl][3] = 1;
+                stVozl++; 
             }else if(vrstica[0] == 'f'){
                 povezave3d[stPove] = [];
                 povezave3d[stPove][0] = parseFloat(vrstica[1]);
@@ -35,86 +39,90 @@ function naloziDatoteko(){
         }
     }
     reader.readAsText(file);
+    izris();
 }
 
-/*
-function izracunajSredinskoTocko(){
-    var vsotaX = 0.0;
-    var vsotaY = 0.0;
-    var vsotaZ = 0.0;
-    var stTock= vozlisca3d.length;
-    for(var i = 0; i < stTock; i++){
-        vsotaX+= vozlisca3d[i][0];
-        vsotaY+= vozlisca3d[i][1];
-        vsotaZ+= vozlisca3d[i][2];
-    }
 
-    sredinskaTocka[0] = vsotaX / stTock;
-    sredinskaTocka[1] = vsotaY / stTock;
-    sredinskaTocka[2] = vsotaZ / stTock;
+function toggleRotate(x,y,z){
+    if(x == true)rotirajX == true? rotirajX = false : rotirajX = true;
+    if(y == true)rotirajY == true? rotirajY = false : rotirajY = true;
+    if(z == true)rotirajZ == true? rotirajZ = false : rotirajZ = true;
+
 }
-*/
-
-
-
-
-
 
 var refreshId = setInterval(function(){
-    rotateZ(0.01);
-    rotateX(0.01);
-    //rotateY(0.01);
+    if(rotirajX == true) rotateX(0.01);
+    if(rotirajY == true) rotateY(0.01);
+    if(rotirajZ == true) rotateZ(0.01);
     izris();
-
-}, 15);
-
-
+    
+    
+}, 16);
 
 
 function rotateX(kot){
-    //izracunajSredinskoTocko();
-    for(var i = 0; i < vozlisca3d.length; i++){
-        var x = vozlisca3d[i][0];
-        var y = vozlisca3d[i][1];
-        var z = vozlisca3d[i][2];
-        vozlisca3d[i][1] = y*Math.cos(kot) - z*Math.sin(kot);
-        vozlisca3d[i][2] = y*Math.sin(kot) + z*Math.cos(kot);
+    for(var i = 0; i < vozlisca4d.length; i++){
+        var x = vozlisca4d[i][0];
+        var y = vozlisca4d[i][1];
+        var z = vozlisca4d[i][2];
+        vozlisca4d[i][1] = y*Math.cos(kot) - z*Math.sin(kot);
+        vozlisca4d[i][2] = y*Math.sin(kot) + z*Math.cos(kot);
 
     }
 }
 function rotateY(kot){
-    for(var i = 0; i < vozlisca3d.length; i++){
-        var x = vozlisca3d[i][0];
-        var y = vozlisca3d[i][1];
-        var z = vozlisca3d[i][2];
-        vozlisca3d[i][2] = z*Math.cos(kot) - x*Math.sin(kot);
-        vozlisca3d[i][0] = z*Math.sin(kot) + x*Math.cos(kot);
+    for(var i = 0; i < vozlisca4d.length; i++){
+        var x = vozlisca4d[i][0];
+        var y = vozlisca4d[i][1];
+        var z = vozlisca4d[i][2];
+        vozlisca4d[i][2] = z*Math.cos(kot) - x*Math.sin(kot);
+        vozlisca4d[i][0] = z*Math.sin(kot) + x*Math.cos(kot);
     }
 }
 function rotateZ(kot){
-    for(var i = 0; i < vozlisca3d.length; i++){
-        var x = vozlisca3d[i][0];
-        var y = vozlisca3d[i][1];
-        var z = vozlisca3d[i][2];
-        vozlisca3d[i][0] = x*Math.cos(kot) - y*Math.sin(kot);
-        vozlisca3d[i][1] = x*Math.sin(kot) + y*Math.cos(kot);
+    for(var i = 0; i < vozlisca4d.length; i++){
+        var x = vozlisca4d[i][0];
+        var y = vozlisca4d[i][1];
+        var z = vozlisca4d[i][2];
+        vozlisca4d[i][0] = x*Math.cos(kot) - y*Math.sin(kot);
+        vozlisca4d[i][1] = x*Math.sin(kot) + y*Math.cos(kot);
     }
 }
 
+function translate1(dy,dx,dz){
+    for(var i = 0; i < vozlisca4d.length; i++){
+        vozlisca4d[i][0] += dx;
+        vozlisca4d[i][1] += dy;
+        vozlisca4d[i][2] += dz;
+    }
+}
+
+function scale(sx,sy,sz){
+    for(var i = 0; i < vozlisca4d.length; i++){
+        vozlisca4d[i][0] *= sx;
+        vozlisca4d[i][1] *= sy;
+        vozlisca4d[i][2] *= sz;
+    }
+}
+
+function perspective(val){
+    razdaljaKamere += val;
+}
+
 function izris(){
-    context.clearRect(0, 0, 1280, 720);//pobrise prejsno sliko
+    context.clearRect(0, 0, platno.width, platno.height);//pobrise prejsno sliko
     var odmikX = platno.width/2;
     var odmikY = platno.height/2; 
 
     var vozlisca2d = [];
     var stVozlisc = 0;
     //preslikava v 2d prostor
-    for(var i = 0; i < vozlisca3d.length; i++){
+    for(var i = 0; i < vozlisca4d.length; i++){
         vozlisca2d[stVozlisc] = [];
-        var x = vozlisca3d[stVozlisc][0];
-        var y = vozlisca3d[stVozlisc][1];
-        var z = vozlisca3d[stVozlisc][2];
-        var scale = razdaljaKamere / (razdaljaKamere + z);
+        var x = vozlisca4d[stVozlisc][0];
+        var y = vozlisca4d[stVozlisc][1];
+        var z = vozlisca4d[stVozlisc][2];
+        var scale = razdaljaKamere / (razdaljaKamere - z);
         /*
         if(z == 0){
             vozlisca2d[stVozlisc][0] = x * razdaljaKamere;
